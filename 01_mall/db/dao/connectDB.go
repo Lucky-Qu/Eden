f1c0e9bc5debec8bad383cf9da6d93a0fb005f3f
LuckyQu <super.lucky.qu@gmail.com>
2024-12-01 04:13:57 +0800

    更新商城内容

--- 01_mall/db/dao/connectDB.go
+++ 01_mall/db/dao/connectDB.go
@@ -2,24 +2,38 @@ package dao
 
 import (
 	"Eden/01_mall/config"
+	"Eden/01_mall/db/model"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 )
 
-// ConnectDB 链接数据库并返回一个db
-func ConnectDB() (db *gorm.DB) {
+var db *gorm.DB
+
+// init 链接数据库并将db存储到包中,并确定数据库中的表是否存在，如果不存在则创建
+func init() {
 	var err error
 	db, err = gorm.Open(mysql.New(mysql.Config{
 		DSN: config.DSN,
 	}), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
 			SingularTable: false,
-			TablePrefix:   "01_mall",
+			TablePrefix:   "01_mall_",
 		},
 	})
 	if err != nil {
 		panic(err)
 	}
-	return db
+	if !db.Migrator().HasTable(&model.Product{}) {
+		err = db.Migrator().CreateTable(&model.Product{})
+		if err != nil {
+			panic(err)
+		}
+	}
+	if !db.Migrator().HasTable(&model.User{}) {
+		err = db.Migrator().CreateTable(&model.User{})
+		if err != nil {
+			panic(err)
+		}
+	}
 }
