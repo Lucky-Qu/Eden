72d4ad452502b02db13f14e1696c61067cee86dc
LuckyQu <super.lucky.qu@gmail.com>
2024-12-01 05:07:51 +0800

    更新商城内容

--- 01_mall/db/dao/user.go
+++ 01_mall/db/dao/user.go
@@ -1,6 +1,37 @@
 package dao
 
-func CreateUser() {}
-func UpdateUser() {}
-func DeleteUser() {}
-func SelectUser() {}
+import (
+	"Eden/01_mall/db/model"
+	"reflect"
+)
+
+// CreateUser 创建新用户
+func CreateUser(user *model.User) {
+	db.Model(&model.User{}).Create(user)
+}
+
+// UpdateUser 更新用户信息
+func UpdateUser() {
+
+}
+
+// DeleteUser 删除用户信息
+func DeleteUser() {
+
+}
+
+// SelectUser 检索用户信息
+func SelectUser(user *model.User) (ResultUser []*model.User) {
+	var queryString = ""
+	v := reflect.ValueOf(user).Elem()
+	for i := 0; i < v.NumField(); i++ {
+		if !v.Field(i).IsZero() {
+			if queryString != "" {
+				queryString += "AND"
+			}
+			queryString = queryString + v.Type().Field(i).Name + " " + "=" + " " + v.Field(i).String()
+		}
+	}
+	db.Where(queryString).Find(&ResultUser)
+	return ResultUser
+}
--- 01_mall/db/model/user.go
+++ 01_mall/db/model/user.go
@@ -1,10 +1,14 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 // User 用户的属性
 type User struct {
 	gorm.Model
 	Username string `json:"username"`
 	Password string `json:"password"`
+	Info     string `json:"info"`
+	Sex      string `json:"sex"`
 }
