ee5a15635326f8855b49ecb6080d5512a2ec9754
LuckyQu <super.lucky.qu@gmail.com>
2024-12-02 03:12:36 +0800

    更新商城内容

--- 01_mall/service/user.go
+++ 01_mall/service/user.go
@@ -1,7 +1,16 @@
 package service
 
-import "github.com/gin-gonic/gin"
+import (
+	"Eden/01_mall/db/dao"
+	"Eden/01_mall/db/model"
+	"github.com/gin-gonic/gin"
+)
 
 func UserRegister(c *gin.Context) {
-
+	var user model.User
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		panic(err)
+	}
+	dao.CreateUser(&user)
 }
