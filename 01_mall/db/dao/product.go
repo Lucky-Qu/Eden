eb174c6686cd616d47a3f8b85605c37351c5114a
LuckyQu <super.lucky.qu@gmail.com>
2024-12-04 05:34:12 +0800

    更新产品功能

--- 01_mall/db/dao/product.go
+++ 01_mall/db/dao/product.go
@@ -1,1 +1,18 @@
 package dao
+
+import "Eden/01_mall/db/model"
+
+func CreateProduct(product *model.Product) {
+	db.Model(&model.Product{}).Create(product)
+}
+func DeleteProduct(num int) {
+	db.Model(&model.Product{}).Where("id = ?", num).Unscoped().Delete(num)
+}
+func UpdateProduct(num int, product *model.Product) {
+	db.Model(&model.Product{}).Where("id = ?", num).Updates(product)
+}
+func GetProduct(queryString string) []model.Product {
+	var products []model.Product
+	db.Model(&model.Product{}).Where(queryString).Find(&products)
+	return products
+}
--- 01_mall/db/model/product.go
+++ 01_mall/db/model/product.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 // Product 商品的属性
 type Product struct {
 	gorm.Model
-	Name   string  `json:"name"`
-	Number int     `json:"number"`
-	Price  float64 `json:"price"`
-	Info   string  `json:"info"`
+	Name  string
+	Price int
+	Stock int
+	Info  string
 }
